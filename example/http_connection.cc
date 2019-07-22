910c898ea5215775a8a5d783ee2fc5d0371fcc87
Valentin Tolmer <valentin.tolmer@gmail.com>
2019-07-22 17:00:50 +0200

    http_connection: Detail the example

--- example/http_connection.cc
+++ example/http_connection.cc
@@ -5,12 +5,46 @@
 
 #include "protenc.h"
 
+// Example use of the ProtEnc library: an advanced builder pattern.
+//
+// Here, we have an HTTP connection builder. We want to force the user to add
+// one or more headers, then exactly one body, after which he will be able to
+// start the connection. ProtEnc will enforce that our object is in the right
+// state at every step.
+//
+// We can represent those constraints as a Finite State Machine (FSM)
+// (https://en.wikipedia.org/wiki/Finite-state_machine), which is equivalent to
+// a regex. In other words, our protocol looks like the regex:
+//   (header)+(body)(start)
+//
+// If we turn that into a FSM, we get:
+//
+//  *******               *********             ******
+//  *START* ---header---> *HEADERS* ---body---> *BODY* -->start<--
+//  *******               *********             ******
+//                         |    ^
+//                         |    |
+//                         header
+//
+// We start in the state "START", from there we can add a header, which leads
+// us to the state "HEADERS". There, we have a choice: we can keep adding
+// headers as many times as we want, always looping back to the same state, or
+// we can add a body, getting us to the state "BODY". From there, we just have
+// to call "start", which is the final transition into an accepting state.
+//
+// The setup consists of:
+//   - An enum to list the states.
+//   - The types describing our state machine (initial states, final states,
+//     transitions, ...).
+//   - The implementation of the builder, unconstrained.
+//   - The wrapper class, which will do the constraining.
 
-// Dummy connection class
+
+// Dummy connection class.
 using HTTPConnection = std::tuple<std::vector<std::string>, std::string>;
 
 
-// Marker class for the state.
+// List of states in our FSM.
 enum class HTTPStarterState {
   // Empty class.
   START,
@@ -21,13 +55,13 @@ enum class HTTPStarterState {
 };
 
 // Forward-declaration of the wrapper, to make it a friend of
+// HTTPConnectionStarter. That way, we can hide the constructor of
+// HTTPConnectionStarter, preventing users from creating an unwrapped
 // HTTPConnectionStarter.
 template <HTTPStarterState>
 class HTTPConnectionStarterWrapper;
 
 // Basic implementation of the class, without the protocol constraints.
-// We want to enforce that the user adds one or more headers, then a body, then
-// start the connection.
 class HTTPConnectionStarter {
  public:
 
@@ -39,11 +73,13 @@ class HTTPConnectionStarter {
       body_ = std::move(body);
   }
 
+  // This is a query function: it will just return information, without
+  // changing the object.
   size_t num_headers() const {
       return headers_.size();
   }
 
-  // Start the connection
+  // Start the connection. This should consume the object.
   HTTPConnection
   start() {
     return HTTPConnection(std::move(headers_), std::move(body_));
@@ -71,15 +107,20 @@ using prot_enc::ValidQueries;
 using prot_enc::ValidQuery;
 using prot_enc::InitialStates;
 
-// Definition of the graph:
+// Definition of the graph, from the initial states, transitions and final
+// states.
+// The use of aliases allows us to get around the limitations of macros with
+// parameters containing commas.
 
 // Initial states (can be a list).
 using MyInitialStates = InitialStates<HTTPStarterState::START>;
 
 // Transitions, of the form <starting state, end state, function pointer>.
 using MyTransitions = Transitions<
+      // We can go from START to HEADERS by calling add_header.
       Transition<HTTPStarterState::START, HTTPStarterState::HEADERS,
                  &HTTPConnectionStarter::add_header>,
+      // This is the loop: we stay in the state HEADERS.
       Transition<HTTPStarterState::HEADERS, HTTPStarterState::HEADERS,
                  &HTTPConnectionStarter::add_header>,
       Transition<HTTPStarterState::HEADERS, HTTPStarterState::BODY,
@@ -94,22 +135,30 @@ using MyFinalStates = FinalStates<
 // Valid information queries, of the form <accepting state, end function
 // pointer>.
 using MyValidQueries = ValidQueries<
+   // We can only call num_headers from the state BODY.
    ValidQuery<HTTPStarterState::BODY, &HTTPConnectionStarter::num_headers>
   >;
 
+// This is the declaration of the wrapper: it is a class declaration.
 PROTENC_START_WRAPPER(HTTPConnectionStarterWrapper, HTTPConnectionStarter,
                       HTTPStarterState, MyInitialStates, MyTransitions,
                       MyFinalStates, MyValidQueries);
 
+  // Declare the list of functions that we are wrapping:
+  // Transitions
   PROTENC_DECLARE_TRANSITION(add_header);
   PROTENC_DECLARE_TRANSITION(add_body);
 
+  // End functions.
   PROTENC_DECLARE_FINAL_STATE(start);
 
+  // Query functions.
   PROTENC_DECLARE_QUERY_METHOD(num_headers);
 
 PROTENC_END_WRAPPER;
 
+// Factory method. Note that trying to build a wrapper in any other state than
+// START (because it's in our initial state list) will fail.
 static HTTPConnectionStarterWrapper<HTTPStarterState::START>
 GetConnectionStarter() {
   return {};
@@ -119,12 +168,13 @@ int main() {
     // Get the connection with an easy interface.
     // The order of the functions is checked at compile-time by the state
     // markers.
-    auto connection_1 = GetConnectionStarter()
-                        .add_header("First header")
-                        .add_header("Second header")
-                        .add_body("Body")
-                        .start();
+    HTTPConnection connection_1 = GetConnectionStarter()
+                                  .add_header("First header")
+                                  .add_header("Second header")
+                                  .add_body("Body")
+                                  .start();
 
+    // Print the body of the connection, just to check that we succeeded.
     std::cout << std::get<1>(connection_1) << '\n';
 
     // The query is only valid once we have the body.
@@ -132,7 +182,16 @@ int main() {
                            .add_header("First header")
                            .add_body("Body");
     std::cout << "Num headers: " << connection_part.num_headers() << '\n';
+    // We have to move the connection_part, because all the operations consume
+    // the object and return a new one.
     auto connection_2 = std::move(connection_part).start();
+
+    // This doesn't compile:
+    // GetConnectionStarter().add_body("Body");
+    // GetConnectionStarter().add_header("First header")
+    //                       .add_body("Body")
+    //                       .add_header("Second header");
+    // GetConnectionStarter().add_header("Header").start();
 
     return 0;
 }
